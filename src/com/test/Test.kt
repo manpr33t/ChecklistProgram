c2a628334c77d9fefc993dfb7a3481d9ea106ab0
manpr33t <manpreetbassi96@gmail.com>
2018-02-20 23:29:26 -0800

    forgot about this

---
+++ src/com/test/Test.kt
@@ -0,0 +1,14 @@
+package com.test
+
+// Messing around with kotlin
+
+fun main(args : Array<String>) {
+    println("Hello World");
+}
+
+class Test : Comparable<Test> {
+    override fun compareTo(other: Test): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}

4182959f87505ffb8a65d470baecfda3d43372a9
manpr33t <manpreetbassi96@gmail.com>
2018-03-07 22:57:28 -0800

    added some bugs to fix later

--- src/com/test/Test.kt
+++ src/com/test/Test.kt
@@ -8,9 +8,10 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.poifs.filesystem.POIFSFileSystem
 
 import java.io.File
+import java.io.PrintWriter
 
 private var spreadSheetRows: ArrayList<HSSFRow>? = null
-private var dataMap: MutableMap<String, String>? = null
+private var dataMap: MutableMap<String, MutableSet<String>>? = null
 
 /**
  * Create a Map from a UCR Excel Spreadsheet
@@ -29,17 +30,49 @@ fun readDataFromFile(inputFile: File) {
 
     while (rowIterator.hasNext()) spreadSheetRows?.add(rowIterator.next() as HSSFRow)
 
-    spreadSheetRows!!.forEach({
-        dataMap!!.put (
-            it.getCell(8).toString(),
-            Utility.removeZipcodePrefix(it.getCell(it.firstCellNum.toInt()).toString())
-    )})
+        spreadSheetRows!!.forEach({
+            if (checkForData(it)) {
+                try {
+                    if (!dataMap!!.containsKey(it.getCell(8).toString()))
+                        dataMap!!.put(it.getCell(8).toString(), HashSet())
+                    dataMap!!.get(it.getCell(8).toString())?.add(
+                            Utility.removeZipcodePrefix(it.getCell(it.firstCellNum.toInt()).toString())
+                    )
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    println(e.localizedMessage)
+                }
+            }
+        })
 
 }
 
-fun getDataMap() : MutableMap<String, String>? {
+fun checkForData(row: HSSFRow): Boolean {
+    return row.getCell(row.firstCellNum.toInt()).toString().startsWith("D") ||
+            row.getCell(row.firstCellNum.toInt()).toString().startsWith("P") ||
+            !row.getCell(row.firstCellNum.toInt()).toString().startsWith("S")
+}
+
+fun getDataMap() : MutableMap<String, MutableSet<String>>? {
     return dataMap
 }
 
+fun outputMap() {
+    val file: File = File("test.csv")
+    val pw = PrintWriter(file)
+
+    dataMap!!.keys.forEach {
+        pw.write(it)
+        dataMap?.get(it)?.forEach { i ->
+            pw.write(i)
+            pw.write(",")
+        }
+        pw.write("\n")
+    }
+    pw.close()
+}
+
 fun main(args: Array<String>) {
+    readDataFromFile(File("UCR.xls"))
+    outputMap()
 }

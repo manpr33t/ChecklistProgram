49c68538934b35a760202c887ed1f66c82167670
manpr33t <manpreetbassi96@gmail.com>
2018-03-08 20:42:55 -0800

    fixed the bugs added earlier

--- src/com/test/Test.kt
+++ src/com/test/Test.kt
@@ -10,69 +10,83 @@ import org.apache.poi.poifs.filesystem.POIFSFileSystem
 import java.io.File
 import java.io.PrintWriter
 
-private var spreadSheetRows: ArrayList<HSSFRow>? = null
-private var dataMap: MutableMap<String, MutableSet<String>>? = null
+class UCRParser {
 
-/**
- * Create a Map from a UCR Excel Spreadsheet
- *
- */
-fun readDataFromFile(inputFile: File) {
 
-    spreadSheetRows = ArrayList()
-    dataMap = HashMap()
+    private var spreadSheetRows: ArrayList<HSSFRow>? = null
+    private var dataMap: MutableMap<String, MutableSet<String>>? = null
 
-    val fileSystem: POIFSFileSystem = POIFSFileSystem(inputFile)
-    val workBook: HSSFWorkbook = HSSFWorkbook(fileSystem)
-    val spreadSheet: HSSFSheet = workBook.getSheetAt(0)
+    /**
+     * Create a Map from a UCR Excel Spreadsheet
+     *
+     */
+    fun readDataFromFile(inputFile: File) {
 
-    val rowIterator: Iterator<Row> = spreadSheet.rowIterator()
+        spreadSheetRows = ArrayList()
+        dataMap = HashMap()
 
-    while (rowIterator.hasNext()) spreadSheetRows?.add(rowIterator.next() as HSSFRow)
+        val fileSystem = POIFSFileSystem(inputFile)
+        val workBook = HSSFWorkbook(fileSystem)
+        val spreadSheet: HSSFSheet = workBook.getSheetAt(0)
 
-        spreadSheetRows!!.forEach({
+        val rowIterator: Iterator<Row> = spreadSheet.rowIterator()
+
+        while (rowIterator.hasNext()) spreadSheetRows?.add(rowIterator.next() as HSSFRow)
+
+        spreadSheetRows!!.forEach({ it ->
             if (checkForData(it)) {
                 try {
-                    if (!dataMap!!.containsKey(it.getCell(8).toString()))
-                        dataMap!!.put(it.getCell(8).toString(), HashSet())
-                    dataMap!!.get(it.getCell(8).toString())?.add(
-                            Utility.removeZipcodePrefix(it.getCell(it.firstCellNum.toInt()).toString())
-                    )
+                    if (it.getCell(8) != null) {
+                        if (!dataMap!!.containsKey(it.getCell(8).toString()))
+                            dataMap!!.put(it.getCell(8).toString(), HashSet())
+
+                        dataMap!![it.getCell(8).toString()]!!.add(
+                                Utility.removeZipcodePrefix(it.getCell(it.firstCellNum.toInt()).toString())
+                        )
+                    }
                 } catch (e: Exception) {
-                    e.printStackTrace()
                     println(e.localizedMessage)
+                    println("Row:${it.rowNum + 1}")
                 }
             }
         })
-
-}
-
-fun checkForData(row: HSSFRow): Boolean {
-    return row.getCell(row.firstCellNum.toInt()).toString().startsWith("D") ||
-            row.getCell(row.firstCellNum.toInt()).toString().startsWith("P") ||
-            !row.getCell(row.firstCellNum.toInt()).toString().startsWith("S")
-}
-
-fun getDataMap() : MutableMap<String, MutableSet<String>>? {
-    return dataMap
-}
-
-fun outputMap() {
-    val file: File = File("test.csv")
-    val pw = PrintWriter(file)
-
-    dataMap!!.keys.forEach {
-        pw.write(it)
-        dataMap?.get(it)?.forEach { i ->
-            pw.write(i)
-            pw.write(",")
-        }
-        pw.write("\n")
     }
-    pw.close()
+
+    fun checkForData(row: HSSFRow): Boolean {
+        return row.getCell(row.firstCellNum.toInt()).toString().startsWith("D") ||
+                row.getCell(row.firstCellNum.toInt()).toString().startsWith("P") ||
+                !row.getCell(row.firstCellNum.toInt()).toString().startsWith("S")
+    }
+
+    fun getDataMap(): MutableMap<String, MutableSet<String>>? {
+        return dataMap
+    }
+
+    fun outputMap() {
+        val file: File = File("DataMap.csv")
+        val pw = PrintWriter(file)
+
+        getDataMap()!!.keys.forEach {
+            pw.write(it)
+            pw.write(",")
+            getDataMap()?.get(it)?.forEach { i ->
+                pw.write(i)
+                pw.write(",")
+            }
+            pw.write("\n")
+        }
+        pw.close()
+    }
+
+    companion object {
+        fun run(s: String) {
+            val p = UCRParser()
+            p.readDataFromFile(File(s))
+            p.outputMap()
+        }
+    }
 }
 
 fun main(args: Array<String>) {
-    readDataFromFile(File("UCR.xls"))
-    outputMap()
+    UCRParser.run("UCR.xls")
 }

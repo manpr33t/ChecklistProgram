54ef6dc6240f35028171b67a68c18524748d1341
manpr33t <manpreetbassi96@gmail.com>
2018-06-28 22:19:09 -0700

    Started work on updater. Looks like I might need to create a login window to get through the proxy firewall

--- ChecklistUpdater/src/net/manpreet_singh/updater/Updater.kt
+++ ChecklistUpdater/src/net/manpreet_singh/updater/Updater.kt
@@ -1,15 +1,29 @@
 package net.manpreet_singh.updater
 
-import javafx.application.Application
-import net.manpreet_singh.gui.MainGUI
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
+import java.io.FileReader
+import java.net.URL
+import org.apache.commons.io.FileUtils
+import java.io.File
+
+
 
 class Updater {
+    class Version(fileName: String) {
+        val version = (JSONParser().parse(FileReader(fileName)) as JSONObject).get("version").toString()
+        val version_major = version.split(".")[0]
+        val version_minor = version.split(".")[1]
+        val version_min = version.split(".")[2]
+    }
     init {
+        val v = Version("dependencies/version.json")
     }
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            Application.launch(MainGUI::class.java)
+            Updater()
+//            Runtime.getRuntime().exec("cmd /c start \"\" Launch.vbs");
         }
     }
 }

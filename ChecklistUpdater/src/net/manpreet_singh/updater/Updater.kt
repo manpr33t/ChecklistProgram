e3ce27250c5b44ed74ba1c07ccbddd35c348df4d
manpr33t <manpreetbassi96@gmail.com>
2018-08-01 20:47:52 -0700

    Fixed botcher refactoring

--- ChecklistUpdater/src/net/manpreet_singh/updater/Updater.kt
+++ ChecklistUpdater/src/net/manpreet_singh/updater/Updater.kt
@@ -3,13 +3,14 @@ package net.manpreet_singh.updater
 import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
 import java.io.FileReader
+import java.net.URL
 
 class Updater {
     class Version(fileName: String) {
         val version = (JSONParser().parse(FileReader(fileName)) as JSONObject).get("version").toString()
         val version_major = version.split(".")[0]
         val version_minor = version.split(".")[1]
-        val version_min = version.split(".")[2]
+        val version_patch = version.split(".")[2]
     }
     init {
         val v = Version("dependencies/version.json")
@@ -17,8 +18,6 @@ class Updater {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            Updater()
-//            Runtime.getRuntime().exec("cmd /c start \"\" Launch.vbs");
         }
     }
 }

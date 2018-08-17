03538133afe0d5848285679b4f31795f55309925
manpr33t <manpreetbassi96@gmail.com>
2018-08-16 18:36:31 -0700

    Added checklist extractor, a little program that extracts a list from a bigger list

---
+++ src/test/java/Checklist Extractor.kt
@@ -0,0 +1,47 @@
+package test.java
+
+import net.manpreet_singh.checklist.Utility
+import net.manpreet_singh.config.UCRParser
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.PrintWriter
+
+class Extractor {
+    init {
+        val outputFile = File("Output.csv")
+        val pw = PrintWriter(outputFile)
+
+        val parser = UCRParser()
+        parser.readDataFromFile(File("UCR.xls"))
+        val ucrData = parser.getDataMap()
+
+        val checklistFile = FileReader("checklists/LVDDispatchChecklistMASTER.csv")
+
+        val br = BufferedReader(checklistFile)
+
+        val iterator = br.lineSequence().iterator()
+        while (iterator.hasNext()) {
+            val line = iterator.next()
+            val buffer = line.split(",")
+            if (ucrData!!["SELGTACO01"]?.contains(Utility.removeZipcodePrefix(buffer[1]))!!) {
+                pw.println("SELGTACO01,${buffer[1]}, ${buffer[2]}")
+            }
+        }
+        pw.close()
+
+        var keyNumbers = 0
+        println("Available Sort Groups: ")
+        ucrData!!.keys.forEach {
+            print(keyNumbers++)
+            println(") $it")
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Extractor()
+        }
+    }
+}

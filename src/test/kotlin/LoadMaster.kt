a2116f3ce818b84ff2d057f9f51408cbaf2b1b68
manpr33t <manpreetbassi96@gmail.com>
2018-09-20 16:47:59 -0700

    Got 'er done

--- src/test/kotlin/LoadMaster.kt
+++ src/test/kotlin/LoadMaster.kt
@@ -1,7 +1,12 @@
 package test.kotlin
 
+import com.sun.org.apache.xml.internal.utils.StringToIntTable
+import net.manpreet_singh.checklist.Utility
 import net.manpreet_singh.config.UCRParser
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
+import java.io.PrintWriter
 
 class LoadMaster {
     private var tripData: MutableMap<String, MutableSet<String>>? = null
@@ -10,14 +15,31 @@ class LoadMaster {
         ucrParser.readDataFromFile(File("UCR.xls"))
         tripData = ucrParser.getTrips()
 
+        val printWriter = PrintWriter(File("Trips.csv"))
+        printWriter.println("Here's what's Loaded")
+
         println("Available Trips: ")
-        var num = 1
         tripData!!.keys.forEach { it ->
-            println("$num) $it")
-            num++
+            println(it)
         }
 
         println("\nYou decide: ")
+        var choice = readLine()
+        choice = choice!!.toDouble().toString()
+        if (tripData!!.containsKey(choice)) {
+            val checklistFile = FileReader("checklists/SpokaneDispatchCheckListMASTER.csv")
+            val br = BufferedReader(checklistFile)
+            val iterator = br.lineSequence().iterator()
+            while (iterator.hasNext()) {
+                val line = iterator.next()
+                val buffer = line.split(",")
+                if (tripData!![choice]?.contains(Utility.removeZipcodePrefix(buffer[1]))!!)
+                    printWriter.println("${buffer[0]},${buffer[1]},${buffer[2]}")
+            }
+        }
+        else
+            printWriter.println("Nothing at all!")
+        printWriter.close()
     }
 
 
